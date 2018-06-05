0ff2284fe02a4b6c07e7f0249437a5cba20be578
YevheniiMoruhyi <zhekamorug@gmail.com>
2018-06-05 09:35:02 +0300

    create file main.py and add all necessary modules

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#Importing all the neccessary modules
+import sys
+import textfsm
+import os
+from pprint import pprint
+import re
+import easysnmp
+import influxdb
+import requests.exceptions
+import time
+import datetime
+import threading
+
