4bc68d2695f6e7cea2b64da07724616f4690eeb5
YevheniiMoruhyi <zhekamorug@gmail.com>
2018-06-05 09:52:21 +0300

    add find_devices() function to main.py

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ import time
 import datetime
 import threading
 
+def find_devices():
+	"""Find all files that ends with .conf extantion"""
+	if os.path.exists(r"./devices") and os.path.isdir(r"./devices"):
+		if os.listdir(r"./devices") != []:
+			global dev_files
+			dev_files = ["./devices/" + file for file in os.listdir(r"./devices") if file.endswith(".conf")]
+		else:
+			print "Folder (./devices) is empty!"
+			sys.exit()
+	else:
+		print "Folder (./devices) does not exists!"
+		sys.exit()
